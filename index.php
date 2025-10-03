3c1aa3138cb476d13d2b8b52a1e087f9a3d258a9
Jorge F Devesa Caria <jorgefcaria@gmail.com>
2025-10-03 12:47:46 +0200

    First commit

---
+++ index.php
@@ -0,0 +1,39 @@
+<?php
+    // for($i = 1; $i<=5; $i++){
+    //     echo "Hello world $i </br>";
+    //     echo "Hello world";
+    //     echo $i;
+    // }
+
+
+    // $animales = array(
+    //                 array(
+    //                     array("Kevin", "Togi"),
+    //                     array("Pepito","Manami"),
+    //                     ),
+    //                 array(
+    //                     array("Otis","Amiguito"),
+    //                     array(""),
+    //                     ),
+    //                 );
+
+    //redondear con 3 metodos para obtener algo como: 
+    //"Este método redondea impares el resultado es:
+
+
+    $valor_redondear = 50.525;
+    
+    $valor_redondear_impar = round($valor_redondear, 2, PHP_ROUND_HALF_ODD);
+    // echo round($valor_redondear, 2, PHP_ROUND_HALF_ODD);
+    echo "Este método redondea a impares, el resultado es $valor_redondear_impar</br>";
+
+    $valor_redondear_par = round($valor_redondear, 2, PHP_ROUND_HALF_EVEN);
+    echo "Este método redondea a pares, el resultado es $valor_redondear_par</br>";
+
+    // $valor_redondear_arriba = round($valor_redondear, 2, PHP_ROUND_HALF_DOWN);
+    echo "Este método redondea hacia arriba, el resultado es " . round($valor_redondear, 2, PHP_ROUND_HALF_DOWN) ."</br>";
+
+    
+    
+?>
+
