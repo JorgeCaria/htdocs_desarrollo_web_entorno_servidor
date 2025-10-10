c888aa49c07c33f18391624255c111533d7eacb6
Jorge F Devesa Caria <jorgefcaria@gmail.com>
2025-10-10 13:20:52 +0200

    p2 u2 ejercicio 3

---
+++ DWES/Practica2U2/p2_u2_ejercicio_3.php
@@ -0,0 +1,21 @@
+<?php
+
+/*
+3. Almacena  en  un  array  los  10  primeros  números  pares.  Imprímelos  cada  uno  en 
+una línea. 
+
+*/
+
+$num = 0;
+$pares_array = array();
+
+while(count($pares_array) < 10){
+   if($num%2==0){
+      $pares_array[] = $num;
+      echo "$num <br>";
+   }
+   $num++;
+}
+
+
+?>
---
+++ DWES/Practica2U2/p2_u2_ejercicio_4.php
@@ -0,0 +1,27 @@
+<?php
+
+/*
+4. 
+Genera una matriz de 4*4 de forma aleatoria con números enteros desordenados 
+mostrar en un renglón los elementos almacenados en la diagonal principal y en el 
+siguiente los de la diagonal secundaria.
+
+*/
+
+$array_numeros_desordenados = array(
+                                       array(2, 4, 6, 8),
+                                       array(6, 8, 1, 9),
+                                       array(0, 1, 4, 7),
+                                       array(3, 5, 7, 9)
+);
+
+$n = 0;
+
+for($i = 0; $i < count($array_numeros_desordenados); $i++){
+   for($j = 0; $j < count($array_numeros_desordenados[$i]); $j++){
+      echo $array_numeros_desordenados[$i][$j] . "\t";
+   }
+   echo "<br>";
+}
+
+?>
