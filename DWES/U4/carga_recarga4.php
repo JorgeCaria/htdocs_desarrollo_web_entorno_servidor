26fe80d64e0fe24feb97ce8365ab9988bfb5694a
Jorge F Devesa Caria <jorgefcaria@gmail.com>
2025-10-17 12:35:12 +0200

    carga y recarga con 2 archivos

--- DWES/U4/carga_recarga4.php
+++ DWES/U4/carga_recarga4.php
@@ -1,13 +1,15 @@
 <?php 
 
-if (isset($_POST['0'])){
-    for ($i=0; $i < 10; $i++) { 
+if (isset($_POST['0'])) {
+    for ($i=0; $i < $_POST['cajas']; $i++) { 
         echo("El número " . $i+1 . " es " . $_POST[$i] . "<br>");
     }
 }else{
+
     echo '<form method="post" action="carga_recarga4.php">';
-    
-    for ($i = 0; $i < 10; $i++) { 
+    echo '<input type="hidden" name="cajas" value="'.$_POST['cajas'].'">';
+
+    for ($i = 0; $i < $_POST['cajas']; $i++) { 
         echo '
             <label for="' . $i . '">Número ' . $i+1 . ':</label>
             <input type="number" id="' . $i . '" name="' . $i . '"><br><br>
---
+++ DWES/U4/carga_recarga5.php
@@ -0,0 +1,11 @@
+<?php 
+
+echo <<<_END
+  <form method="post" action="carga_recarga4.php">
+        <label>Número de cajas:</label>
+        <input type="number" id="cajas" name="cajas"><br><br>
+        <button type="submit" name="bton">Mostrar</button>
+    </form>
+_END;
+
+?> 
